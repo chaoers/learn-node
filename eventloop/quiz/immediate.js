818d13fe84bcbb4c349958ae09159213f079bb54
Bryant-surface <chaoers@bu.edu>
2024-02-26 16:28:23 -0500

    class activity

--- eventloop/quiz/immediate.js
+++ eventloop/quiz/immediate.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 
+setImmediate(() => {
+  console.log('I was scheduled to run immediately');
+});
+
 fs.readFile('./data/test.txt', function(err, data) {
+
   const startCallback = Date.now();
   // do something that will take 10ms...
   while (Date.now() - startCallback < 10) {
@@ -16,8 +21,6 @@ fs.readFile('./data/test.txt', function(err, data) {
     console.log(`${delay}ms have passed since I was scheduled`);
   }, 5);
 
-  setImmediate(() => {
-    console.log('I was scheduled to run immediately');
-  });
+
 });
 
